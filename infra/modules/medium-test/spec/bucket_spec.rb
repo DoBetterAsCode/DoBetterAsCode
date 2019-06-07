7faffe2c8e24ae420f8554a6ce0f2d598c5166dc
Effy Elden <git@effy.is>
2019-06-07 13:10:58 +1000

    Improve bucket_spec to check policy with regex

--- infra/modules/medium-test/spec/bucket_spec.rb
+++ infra/modules/medium-test/spec/bucket_spec.rb
@@ -7,21 +7,15 @@ outputs = eval(ENV['KITCHEN_KITCHEN_TERRAFORM_OUTPUT'])
 describe s3_bucket(outputs[:bucket_name][:value]), region: 'ap-southeast-2' do
   it { should exist }
   it { should have_acl_grant(grantee: 'http://acs.amazonaws.com/groups/global/AllUsers', permission: 'READ') }
-  it {
-    should have_policy <<-POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowPublicRead",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::#{outputs[:bucket_name][:value]}/*"
+  it "should have public read policy for all objects" do
+    policy = JSON.parse(subject.policy.policy.read)
+    public_read_statement = policy["Statement"].find { |p|
+      p["Effect"] == "Allow" &&
+      p["Principal"] == "*" &&
+      p["Action"] == "s3:GetObject" &&
+      /\/*$/ =~ p["Resource"]
     }
-  ]
-}
-    POLICY
-  }
+    expect(public_read_statement).not_to be_nil
+  end
 end
 
