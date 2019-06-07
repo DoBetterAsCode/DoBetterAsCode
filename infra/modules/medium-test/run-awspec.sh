911168c40a9bc05ac9c689a3050c352fd3ac8593
Effy Elden <git@effy.is>
2019-06-07 14:28:03 +1000

    Add 20 min delay when running full medium tests between testing and destroying

--- infra/modules/medium-test/run-awspec.sh
+++ infra/modules/medium-test/run-awspec.sh
@@ -5,4 +5,5 @@ cd ${TERRAFORM_DIR} || exit
 terraform init
 kitchen converge $1
 kitchen verify $1
+[[ -z "$1" ]] && sleep 1200 # Sleep if running all suites to give CloudFront time to deploy so it can be destroyed
 kitchen destroy $1
