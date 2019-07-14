1206fe5fa6cd83de6985f5b311d472550a3b3f50
Effy Elden <git@effy.is>
2019-07-14 11:01:58 +1000

    Add shellcheck, tweak yamllint config, fix shellcheck issues in scripts

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 echo "Syncing to s3 bucket: dobetterascode-${ENVIRONMENT}"
-aws s3 sync public/ s3://dobetterascode-${ENVIRONMENT} --delete
+aws s3 sync public/ "s3://dobetterascode-${ENVIRONMENT}" --delete
--- infra/run.sh
+++ infra/run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 echo "Running for project ${TERRAFORM_DIR}"
-cd ${TERRAFORM_DIR} || exit
+cd "${TERRAFORM_DIR}" || exit
 
 echo "Creating .terraformrc in ${HOME}"
 echo "credentials \"app.terraform.io\" { token = \"${TF_TOKEN}\" }" > ~/.terraformrc
 
-export TF_WORKSPACE=$TF_VAR_env
+export TF_WORKSPACE=${TF_VAR_env:?}
 
 echo "Initialising Terraform"
 terraform init
 
 echo "Changing to Terraform workspace"
-terraform workspace select ${TF_VAR_env} || terraform workspace new ${TF_VAR_env}
+terraform workspace select "${TF_VAR_env}" || terraform workspace new "${TF_VAR_env}"
 
 echo "Running Terraform operation ${TF_OPERATION}"
-terraform ${TF_OPERATION} -auto-approve
+terraform "${TF_OPERATION}" -auto-approve
