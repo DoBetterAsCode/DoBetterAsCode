cb9af1f24e798f205a181f18be6255e3f8c48588
Shohre <shohre.mansouri@rea-group.com>
2019-07-14 13:28:32 +1000

    Refactor s3_bucket test

--- infra/modules/integration-test/spec/s3_bucket_spec.rb
+++ infra/modules/integration-test/spec/s3_bucket_spec.rb
@@ -7,17 +7,19 @@ tf_state = Helpers::State::load_state_for_module("s3_bucket")
 
 bucket_name = tf_state['modules'][0]['outputs']['bucket_name']['value']
 
+def is_public_read_policy(p)
+  p["Effect"] == "Allow" &&
+  p["Principal"] == "*" &&
+  p["Action"] == "s3:GetObject" &&
+  /\/*$/ =~ p["Resource"]
+end
+
 describe s3_bucket(bucket_name), region: 'ap-southeast-2' do
   it { should exist }
   it { should have_acl_grant(grantee: 'http://acs.amazonaws.com/groups/global/AllUsers', permission: 'READ') }
   it "should have public read policy for all objects" do
     policy = JSON.parse(subject.policy.policy.read)
-    public_read_statement = policy["Statement"].find { |p|
-      p["Effect"] == "Allow" &&
-      p["Principal"] == "*" &&
-      p["Action"] == "s3:GetObject" &&
-      /\/*$/ =~ p["Resource"]
-    }
+    public_read_statement = policy["Statement"].find { |p| is_public_read_policy(p) }
     expect(public_read_statement).not_to be_nil
   end
 end
