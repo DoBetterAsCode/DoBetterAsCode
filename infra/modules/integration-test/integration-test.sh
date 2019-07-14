fbb7bf14028305f3db9d447d0714b465944cdff5
Shohre <shohre.mansouri@rea-group.com>
2019-07-14 11:18:32 +1000

    Rename unit-test to integration-test

--- infra/modules/integration-test/integration-test.sh
+++ infra/modules/integration-test/integration-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cd ${TERRAFORM_DIR} || exit
+cd "${TERRAFORM_DIR}" || exit
 
 terraform init
-kitchen converge $1
-kitchen verify $1
+kitchen converge "$1"
+kitchen verify "$1"
 [[ -z "$1" ]] && sleep 1200 # Sleep if running all suites to give CloudFront time to deploy so it can be destroyed
-kitchen destroy $1
+kitchen destroy "$1"
